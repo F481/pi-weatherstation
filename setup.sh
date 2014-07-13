8ee50e979eee02f9fb4bb91ba070561e0962457f
Fabian Frick <fabi.kcirf@gmail.com>
2014-07-13 15:26:46 +0200

    fix pwd command

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-workingdir=pwd
+pwd=`pwd`
 
 # install packages
+sudo apt-get update
 sudo apt-get install php5 sqlite3 php5-sqlite build-essential python-dev python-smbus i2c-tools
 
 # get composer
@@ -14,5 +15,5 @@ fi
 
 php composer.phar install
 cd ./vendor/adafruit/bmp/ && sudo python setup.py install
-cd $workingdir/vendor/adafruit/dht/ && sudo setup.py install
-cd $workingdir/ && sqlite3 /db/pi-weatherstation.db < schema.sql
+cd $pwd/vendor/adafruit/dht/ && sudo setup.py install
+cd $pwd/ && sqlite3 /db/pi-weatherstation.db < schema.sql
