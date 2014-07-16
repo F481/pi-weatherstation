09c81493ead70f0f7a4f473bd7ddd69da2e51c21
Fabian Frick <fabi.kcirf@gmail.com>
2014-07-16 23:09:10 +0200

    fix path to schema.sql file

--- setup.sh
+++ setup.sh
@@ -3,17 +3,21 @@
 pwd=`pwd`
 
 # install packages
+echo "Installing system packages..."
 sudo apt-get update
 sudo apt-get install php5 sqlite3 php5-sqlite build-essential python-dev python-smbus i2c-tools
 
 # get composer
-if command -v "curl" > /dev/null; then
+echo "Installing Composer..."
+if command -v "curl" > /dev/null ; then
     curl -sS https://getcomposer.org/installer | php
 else
     php -r "readfile('https://getcomposer.org/installer');" | php
 fi
 
+echo "run command: php composer.phar install"
 php composer.phar install
+
 cd ./vendor/adafruit/bmp/ && sudo python setup.py install
 cd $pwd/vendor/adafruit/dht/ && sudo setup.py install
-cd $pwd/ && sqlite3 /db/pi-weatherstation.db < schema.sql
+cd $pwd/ && sqlite3 /db/pi-weatherstation.db < /db/schema.sql
