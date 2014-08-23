602f87b076db352241f34969b284206a28358729
Fabian Frick <fabi.kcirf@gmail.com>
2014-08-23 19:05:46 +0200

    updated check for sensors

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ source pi-weatherstation.cfg
 pwd=`pwd`
 
 function pause {
-    sleep 3
+    sleep 1
 }
 
 # testing dht
@@ -13,10 +13,9 @@ pause
 
 cd ${pwd}/vendor/adafruit/dht/examples/
 dht_output=$(sudo ./AdafruitDHT.py ${sensor_dht} ${dht_pin})
-#dht_output="Temp=21.1*C  Humidity=50.5%"
 
 if echo ${dht_output} | grep -q ".*Temp.*Hum.*"; then
-  echo "DHT sensor is working!";
+  echo "DHT sensor is working: ${dht_output}";
   pause
 else
   echo "DHT sensor isn't working!";
@@ -30,7 +29,7 @@ pause
 cd ${pwd}/vendor/adafruit/bmp/examples/
 bmp_output=$(sudo python simpletest.py)
 if echo ${bmp_output} | grep -q ".*Temp.*Pre.*Alt.*Sea.*"; then
-  echo "BMP sensor is working!";
+  echo "BMP sensor is working: ${bmp_output}";
   pause
 else
   echo "BMP sensor isn't working!";
