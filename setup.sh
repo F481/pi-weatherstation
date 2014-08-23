b0e9ce748b315be2bbdabcc2b6b4c570fa070c30
Fabian Frick <fabi.kcirf@gmail.com>
2014-08-23 15:20:12 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,36 +1,64 @@
 #!/bin/bash
 
 pwd=`pwd`
-blacklistPath="/etc/modprobe.d/raspi-blacklist.conf"
+blacklistConfPath="/etc/modprobe.d/raspi-blacklist.conf"
+etcModulesPath="/etc/modules"
+
+function pause {
+    echo -n "."
+    sleep 1
+    echo -n "."
+    sleep 1
+    echo "."
+    sleep 1
+}
 
 # install packages
-echo "Installing system packages..."
+echo -n "Install system packages"
+pause
+
 sudo apt-get update
 sudo apt-get install php5 sqlite3 php5-sqlite build-essential python-dev python-smbus i2c-tools
 
+echo -n "Setting up environment"
+pause
+
+# enable i2c modules
+if [ -f ${etcModulesPath} ]; then
+    sudo echo "i2c-bcm2708\ni2c-dev" >> ${etcModulesPath}
+fi
+
+# comment out blacklist
+if [ -f ${blacklistConfPath} ]; then
+    sudo perl -pi -e 's/blacklist spi-bcm2708/#blacklist spi-bcm2708/g' ${blacklistConfPath}
+    sudo perl -pi -e 's/blacklist i2c-bcm2708/#blacklist i2c-bcm2708/g' ${blacklistConfPath}
+fi
+
+echo -n "Setting up Composer"
+pause
+
 # get composer
-echo "Setting up Composer..."
 if command -v "curl" > /dev/null ; then
     curl -sS https://getcomposer.org/installer | php
 else
     php -r "readfile('https://getcomposer.org/installer');" | php
 fi
-
 php composer.phar install
 
-echo "Setting up sensor libraries"
-cd ./vendor/adafruit/bmp/ && sudo python setup.py install
-cd $pwd/vendor/adafruit/dht/ && sudo python setup.py install
-cd $pwd/ && sqlite3 ./db/pi-weatherstation.db < ./db/schema.sql
+echo -n "Setting up sensor libraries"
+pause
 
-if [ -f $blacklistPath ]; then
-    read -r -p "Are you sure? [y/N] " response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
-    then
-        do_something
-    else
-        do_something_else
-    fi
-    read -r -p "To make the sensors working, you have to comment out the two following lines: '#blacklist spi-bcm2708' and '#blacklist spi-bcm2708' [Press ENTER]"
-    sudo nano $blacklistPath
+# sensor libraries
+cd ./vendor/adafruit/bmp/ && sudo python setup.py install
+cd ${pwd}/vendor/adafruit/dht/ && sudo python setup.py install
+cd ${pwd}/ && sqlite3 ./db/pi-weatherstation.db < ./db/schema.sql
+
+#reboot
+read -p "System needs to be rebooted, reboot now? [y/n]" -n 1 -r REPLY
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo reboot
+else
+    echo "You have to reboot the system before use pi-weatherstation"
 fi
