3d57651a7ef53faa32f4def20188a8c5145bffc9
Fabian Frick <fabi.kcirf@gmail.com>
2014-08-23 19:09:35 +0200

    formatted output

--- check.sh
+++ check.sh
@@ -4,18 +4,18 @@ source pi-weatherstation.cfg
 pwd=`pwd`
 
 function pause {
-    sleep 1
+    sleep 2
 }
 
 # testing dht
 echo "Testing DHT: sudo ./vendor/adafruit/dht/examples/AdafruitDHT.py ${sensor_dht} ${dht_pin}"
-pause
 
 cd ${pwd}/vendor/adafruit/dht/examples/
 dht_output=$(sudo ./AdafruitDHT.py ${sensor_dht} ${dht_pin})
 
 if echo ${dht_output} | grep -q ".*Temp.*Hum.*"; then
-  echo "DHT sensor is working: ${dht_output}";
+  echo "DHT sensor is working:";
+  echo ${dht_output}
   pause
 else
   echo "DHT sensor isn't working!";
@@ -24,14 +24,12 @@ fi
 
 # testing bmp
 echo "Testing BMP: sudo python ./vendor/adafruit/bmp/examples/simpletest.py"
-pause
 
 cd ${pwd}/vendor/adafruit/bmp/examples/
 bmp_output=$(sudo python simpletest.py)
 if echo ${bmp_output} | grep -q ".*Temp.*Pre.*Alt.*Sea.*"; then
-  echo "BMP sensor is working: ${bmp_output}";
-  pause
+  echo "BMP sensor is working:";
+  echo ${bmp_output}
 else
   echo "BMP sensor isn't working!";
-  pause
 fi
