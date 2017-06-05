92f39ef7943e49c486805669b1fd3562e93376f5
Fabian F <fabi.kcirf@gmail.com>
2017-06-05 10:30:00 +0200

    added time information

--- lib/getWeatherData.py
+++ lib/getWeatherData.py
@@ -15,18 +15,15 @@ weather_data = {}
 data_point_temp = []
 data_point_hum = []
 data_point_pres = []
-data_point_time = []
 
 for row in c.fetchall():
 
-        data_point_temp.append(round(row[1], 1))
-        data_point_hum.append(round(row[2], 1))
-        data_point_pres.append(round(row[3], 1))
-        data_point_time.append(row[4])
+        data_point_temp.append([round(row[1], 1), row[4]])
+        data_point_hum.append([round(row[2], 1), row[4]])
+        data_point_pres.append([round(row[3]/1000, 1), row[4]])
 
 weather_data["temperature"] = data_point_temp
 weather_data["humidity"] = data_point_hum
 weather_data["pressure"] = data_point_pres
-weather_data["timestamp"] = data_point_time
 
 print json.dumps(weather_data)
